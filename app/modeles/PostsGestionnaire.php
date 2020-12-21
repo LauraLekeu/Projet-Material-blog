aa3c7996601557d7601fb8c242428239b9bc7741
laura <lauralekeu13@gmail.com>
2020-12-21 20:29:09 +0100

    Le gestionnaire générique

--- app/modeles/PostsGestionnaire.php
+++ app/modeles/PostsGestionnaire.php
@@ -8,32 +8,11 @@
  namespace App\Modeles;
  use \Noyau\Classes\App;
 
- class PostsGestionnaire {
-   
-  public function findOneById(int $id =6) {
-    $sql = "SELECT *
-            FROM posts
-            WHERE id = :id;";
-    $rs = App::getConnexion()->prepare($sql);
-    $rs->bindValue(':id', $id, \PDO::PARAM_INT);
-    $rs->execute();
-    return new Post($rs->fetch(\PDO::FETCH_ASSOC));
-  }
+ class PostsGestionnaire extends \Noyau\Classes\GestionnaireGenerique {
 
+   public function __construct() {
+      $this->_table = 'posts';
+      $this->_class = '\App\Modeles\Post';
+   }
 
-  public function findAll() {
-   $sql = "SELECT *
-           FROM posts
-           ORDER BY id ASC
-           LIMIT 3;";
-   $rs = App::getConnexion()->prepare($sql);
-   $rs->execute();
-   $posts = $rs->fetchAll(\PDO::FETCH_ASSOC);
-
-   $tab = [];
-   foreach($posts as $post):
-      $tab[] = new Post($post);
-   endforeach;
-   return $tab;
- }
  }
---
+++ noyau/classes/GestionnaireGenerique.php
@@ -0,0 +1,53 @@
+<?php
+/*
+
+      ./noyau/classes/GestionnaireGenerique.php 
+      Classes de l'application
+
+ */
+
+namespace Noyau\Classes;
+use \Noyau\Classes\App;
+
+abstract class GestionnaireGenerique {
+
+   protected $_tableName, $_class;
+
+   // METHODES CRUD 
+
+   public function findOneById(int $id) {
+      $sql = "SELECT *
+              FROM `{$this->_table}`
+              WHERE id = :id;";
+      $rs = App::getConnexion()->prepare($sql);
+      $rs->bindValue(':id', $id, \PDO::PARAM_INT);
+      $rs->execute();
+      return new $this->_class($rs->fetch(\PDO::FETCH_ASSOC));
+    }
+
+    public function findAll() {
+      $sql = "SELECT *
+              FROM `{$this->_table}`
+              ORDER BY id ASC
+              LIMIT 3;";
+      $rs = App::getConnexion()->prepare($sql);
+      $rs->execute();
+      $tab = $rs->fetchAll(\PDO::FETCH_ASSOC);
+   
+      
+      return $this->fromAssocToObject($tab, $this->_class);
+    }
+
+
+
+   // AUTRES METHODES 
+
+   protected function fromAssocToObject(array $rs, string $class) { // La fonction va convertir un tableau rs en objet de type 'Post' 
+      $tab = [];
+      foreach($rs as $r):
+         $tab[] = new $this->_class($r);
+      endforeach;
+      return $tab;
+   } 
+
+}
